804238470dec7fd573775a27c0f49afb5f69bbce
elizabethxlee <39136626+elizabethxlee@users.noreply.github.com>
2018-05-09 13:18:56 -0700

    Add files via upload

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+
+    Private Sub Add(ByVal decNumber As Decimal, ByVal decNumber2 As Decimal)
+        Dim decAnswer As Decimal = 0
+        decAnswer = decNumber + decNumber2
+        lblDisplay.text = "Answer: " + decAnswer.ToString
+    End Sub
+
+
+    Private Sub Subtract(ByVal decNumber As Decimal, ByVal decNumber2 As Decimal)
+        Dim decAnswer As Decimal = 0
+        decAnswer = decNumber - decNumber2
+        lblDisplay.text = "Answer: " + decAnswer.ToString
+
+    End Sub
+
+    Private Sub Multiply(ByVal decNumber As Decimal, ByVal decNumber2 As Decimal)
+        Dim decAnswer As Decimal = 0
+        decAnswer = decNumber * decNumber2
+        lblDisplay.text = "Answer: " + decAnswer.ToString
+    End Sub
+
+
+    Private Sub Divide(ByVal decNumber As Decimal, ByVal decNumber2 As Decimal)
+        Dim decAnswer As Decimal = 0
+        decAnswer = decNumber / decNumber2
+        lblDisplay.text = "Answer: " + decAnswer.ToString
+    End Sub
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
